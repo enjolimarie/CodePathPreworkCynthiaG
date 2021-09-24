10b118e3477051c7a26c019b3dce2d246c841516
Cynthia Gonzalez <enjolimarie23@gmail.com>
2021-09-23 21:17:52 -0400

    Add-ons for Prework

--- CodePathPrework/ViewController.swift
+++ CodePathPrework/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var billAmountTextField: UITextField!
-
-
     @IBOutlet var tipAmountLabel: UILabel!
-
     @IBOutlet weak var tipControl: UISegmentedControl!
-
     @IBOutlet weak var totalLabel: UILabel!
-
+    @IBOutlet weak var PartyAmount: UISlider!
+    @IBOutlet weak var SplitAmount: UILabel!
+    @IBOutlet weak var SplitPeopleCount: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tipAmountLabel.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
 
@@ -28,9 +27,18 @@ class ViewController: UIViewController {
         let tipPercentages = [0.15,0.18,0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total=bill+tip
+        let split = Int(PartyAmount.value)
+        let splitAgain = Double(split)
+        let totalSplit = total/splitAgain
+
 
         tipAmountLabel.text=String(format: "$%.2f",tip)
         totalLabel.text=String(format: "$%.2f",total)
+        SplitAmount.text=String(format: "$%.2f",totalSplit)
+    }
+  @IBAction func AmountofPeople(_ sender: Any){
+      SplitPeopleCount.text="Split: \(Int(PartyAmount.value))"
+
     }
 
 }
